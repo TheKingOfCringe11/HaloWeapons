7320fb80d83cfcb383ece01e75eff9a9c56ea47f
TheKingOfCringe11 <95687516+TheKingOfCringe11@users.noreply.github.com>
2022-09-21 01:35:48 +0100

    Added text scroll

--- src/UI/UITile.cs
+++ src/UI/UITile.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Linq;
+using System.Text;
 
 namespace DuckGame.HaloWeapons
 {
@@ -11,7 +11,14 @@ namespace DuckGame.HaloWeapons
         };
 
         private EventHandler _click;
-        private int _blinkTimer;
+
+        private float _textScrollTimer;
+        private float _textScrollPauseTimer;
+        private int _textScrollIndex;
+
+        private float _blinkTimer;
+
+        private string _text;
 
         public event EventHandler Click
         {
@@ -26,10 +33,31 @@ namespace DuckGame.HaloWeapons
             }
         }
 
-        public string Text { get; set; }
+        public string Text
+        {
+            get
+            {
+                return _text;
+            }
+
+            set
+            {
+                if (_text == value)
+                    return;
+
+                _text = value;
+
+                _textScrollTimer = 0;
+                _textScrollPauseTimer = 0;
+            }
+        }
+
         public Vec2 Position { get; set; }
         public float Width { get; set; }
         public float Height { get; set; }
+        public float TextScrollInterval { get; set; } = 0.2f;
+        public float TextScrollPauseTime { get; set; } = 1f;
+        public int TextScrollGap { get; set; } = 3;
         public Color Color { get; set; } = Color.LightGray;
         public Color BorderColor { get; set; } = UI.MenuItemColor;
         public Sprite Sprite { get; set; }
@@ -49,18 +77,20 @@ namespace DuckGame.HaloWeapons
             if (Sprite is not null)
                 Graphics.Draw(Sprite, centerX - Sprite.width / 2f, Position.y + Height / 2f - Sprite.height / 2f, 3f);
 
+            string text = GetDisplayedText();
+
             if (!string.IsNullOrEmpty(Text))
-                _font.Draw(Text, new Vec2(centerX - _font.GetWidth(Text) / 2f, Position.y + Height - _font.height - 3f), Color.Black, 3f);
+                _font.Draw(text, new Vec2(centerX - _font.GetWidth(text) / 2f + 0.25f, Position.y + Height - _font.height - 3f), Color.Black, 3f);
 
             if (!Enabled)
             {
                 Graphics.DrawRect(rectangle, Color.Gray * 0.5f, 3f);
             }
-            else if (_blinkTimer > 0)
+            else if (_blinkTimer > 0f)
             {
                 Graphics.DrawRect(rectangle, Color.White * 0.7f, 3f);
 
-                _blinkTimer--;
+                _blinkTimer -= 0.01f;
             }
         }
 
@@ -69,9 +99,70 @@ namespace DuckGame.HaloWeapons
             if (!Enabled)
                 return;
 
-            _blinkTimer = 4;
+            _blinkTimer = 0.04f;
 
             _click?.Invoke(this, EventArgs.Empty);
+        }
+
+        private string GetDisplayedText()
+        {
+            if (TextFits(_text))
+                return _text;
+
+            int length = _text.Length;
+
+            if (_textScrollPauseTimer <= 0f)
+            {
+                if (_textScrollTimer <= 0f)
+                {
+                    _textScrollIndex += 1;
+
+                    if (_textScrollIndex >= length + TextScrollGap)
+                    {
+                        _textScrollIndex = 0;
+                        _textScrollPauseTimer = TextScrollPauseTime;
+                    }
+
+                    _textScrollTimer = TextScrollInterval;
+                }
+                else
+                {
+                    _textScrollTimer -= 0.01f;
+                }
+            }
+            else
+            {
+                _textScrollPauseTimer -= 0.01f;
+            }
+
+            var builder = new StringBuilder();
+            int index = _textScrollIndex;
+
+            while (TextFits(builder.ToString()))
+            {
+                if (index >= length)
+                {
+                    int difference = index - length;
+
+                    if (difference < TextScrollGap)
+                        builder.Append(" ");
+                    else
+                        builder.Append(_text[difference - TextScrollGap]);
+                }
+                else
+                {
+                    builder.Append(_text[index]);
+                }
+
+                index++;
+            }
+
+            return builder.ToString();  
+        }
+
+        private bool TextFits(string text)
+        {
+            return _font.GetWidth(text) <= Width - 4.5f;
         }
     }
 }
